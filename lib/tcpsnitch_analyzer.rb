552fa71713735a8cfea7394522b615445fe41562
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2017-01-19 18:20:29 +0100

    Working on rubocop errors

--- lib/tcpsnitch_analyzer.rb
+++ lib/tcpsnitch_analyzer.rb
@@ -6,13 +6,42 @@ require 'tcpsnitch_analyzer/proportion_stat'
 require 'tcpsnitch_analyzer/time_serie_stat'
 
 module TcpsnitchAnalyzer
-  EXECUTABLE = 'tcpsnitch_analyzer'
-  VERSION = '0.0.1'
+  EXECUTABLE = 'tcpsnitch_analyzer'.freeze
+  VERSION = '0.0.1'.freeze
   
   # Configure Oj
   Oj.default_options = { symbol_keys: true }
 
   class << self
+    def parse_json_object(line)
+      return Oj.load(line.chomp(",\n")) # Remove ',\n' after JSON object
+    rescue => e
+      error(e)
+    end
+
+    def extract_val(hash, node_path)
+      return node_val(hash, node_path)
+    rescue
+      error("invalid -n argument: '#{node_path}'")
+    end
+
+    def compute_val(analysis_type, val, timestamp)
+      if analysis_type == TimeSerieStat
+        analysis_type.add_point(node_val(timestamp, val))
+      else
+        options.analysis_type.add_val(val)
+      end
+    end
+
+    def output_results(options, values_count)
+      puts_options_header(options)
+      if values_count > 0
+        options.analysis_type.print(options)
+      else
+        puts "No data point matching criterias."
+      end
+    end
+    
     def process_files(options, files)
       # We DO NOT want to read the entire JSON files into memory.
       # We DO NOT want to build a Ruby object for the entire JSON array.
@@ -20,48 +49,25 @@ module TcpsnitchAnalyzer
       # Instead, we want to the file line by line, where each line consists of 
       # a single event. We then instantiate each event individually and discard 
       # them as we consume the file, thus giving O(1) memory consumption.
-      matched_data = false
+      values_count = 0
 
       files.each do |file|
         # IO.each should not read entire file in memory. To verify?
         File.open(file).each_with_index do |line, index|
-          next if index == 0    # First line is opening '[' of JSON Array
-          next if line.eql? "]" # Last line is closing ']' of JSON Array
+#          next if (line.eql? "[" or line.eql? "]") 
+          hash = parse_json_object(line)
 
-          # Parse JSON object
-          begin 
-            hash = Oj.load(line.chomp(",\n")) # Remove ',\n' after JSON object
-          rescue Exception => e 
-            error(e)
-          end
-         
-          # Skip if filter does not match
-          next unless filter(hash, options.event_filter)  
-          matched_data = true
-          
-          # Extract value
-          begin
-            val = node_val(hash, options.node_path)
-          rescue Exception => e
-            error("invalid -n argument: '#{options.node_path}'")
-          end
+          next unless filter(hash, options.event_filter)  # Skip by filter?
+          values_count += 1
 
-          # Compute on value
-          if options.analysis_type == TimeSerieStat
-            options.analysis_type.add_point(node_val(hash, 'timestamp'), val)
-          else
-            options.analysis_type.add_val(val)
-          end
+          val = extract_val(hash, options.node_path)
+          timestamp = node_val(hash, 'timestamp')
+
+          compute_val(options.analysis_type, val, timestamp) 
         end
       end
 
-      # Output results
-      puts_options_header(options)
-      if matched_data 
-        options.analysis_type.print(options)
-      else
-        puts "No data point matching criterias."
-      end
+      output_results(options, values_count)
     end
     
     def error(msg)
